ad3eda894918d6afd7644193230efc8ae133fe2a
oringjea@gmail.com <marialuisatinaja@gmail.com>
2023-11-21 20:38:42 +0800

    commit

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View} from "react-native";
+import Icon from "react-native-vector-icons/Entypo";
+import { Guest } from "./src/Data";
+import { FlatList } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.main}>
+      <View style={styles.content}>
+        <FlatList
+          data={Guest}
+          renderItem={({ item }) => (
+            <View style={styles.row}>
+              <View style={styles.initial}>
+                <Text style={styles.initialText}>
+                  {item.name.lname.charAt(0).toUpperCase()}
+                  {item.name.fname.charAt(1).toUpperCase()}
+                </Text>
+              </View>
+              <View style={styles.details}>
+                <Text style={styles.thename}>
+                  {item.name.lname}, {item.name.fname}
+                  {item.name.mname ? `, ${item.name.mname}.` : ""}
+                </Text>
+                <Text style={styles.address}>{item.address}</Text>
+              </View>
+              <Icon name="dots-three-horizontal" size={35} color="black" />
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    paddingBottom: 10,
+  },
+
+  content: {
+    justifyContent: "flex-end",
+  },
+  thename: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  address: {
+    color: "black",
+    fontSize: 15,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+    marginBottom: 15,
+  },
+  initial: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#329ad4",
+    borderRadius: 25,
+    width: 50,
+    height: 50,
+  },
+  initialText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  details: {
+    flex: 1,
+    marginLeft: 10,
   },
 });
---
+++ src/Data.js
@@ -0,0 +1,214 @@
+import react from "react";
+
+export const Guest = [
+    {
+      name: {
+        fname: "MARIA LUISA",
+        mname: "R.",
+        lname: "TINAJA",
+      },
+      address: "MADRID, SPAIN",
+    },
+    {
+      name: {
+        fname: " JEA",
+        mname: "P",
+        lname: "ORING",
+      },
+      address: "PARIS, FRANCE",
+    },
+    {
+      name: {
+        fname: " ANNA",
+        mname: "",
+        lname: "ANIB",
+      },
+      address: "BERLIN, GERMANY",
+    },
+    {
+      name: {
+        fname: " GELO",
+        mname: "",
+        lname: "HIBAYA",
+      },
+      address: "ROME, ITALY",
+    },
+    {
+      name: {
+        fname: "MELROSE",
+        mname: "D",
+        lname: "VALIENTE",
+      },
+      address: "BARCELONA, SPAIN",
+    },
+    {
+      name: {
+        fname: " TASH",
+        mname: "",
+        lname: "RAN",
+      },
+      address: "OSLO, NORWAY",
+    },
+    {
+      name: {
+        fname: " JOHNMARK",
+        mname: "Q",
+        lname: "LAGRIMAS",
+      },
+      address: "HELSINKI, FINLAND",
+    },
+    {
+      name: {
+        fname: " JESS",
+        mname: "",
+        lname: "TONG",
+      },
+      address: "STOCKHOLM, SWEDEN",
+    },
+    {
+      name: {
+        fname: " LUI",
+        mname: "R",
+        lname: "LIM",
+      },
+      address: " PERM, RUSSIA",
+    },
+    {
+        name: {
+          fname: "ALBERT",
+          mname: "D",
+          lname: "ANIB",
+        },
+        address: "URUMQI, CHINA",
+      },
+      {
+        name: {
+          fname: "PEARL",
+          mname: "",
+          lname: "TAN",
+        },
+        address: "DUND-US, MONGOLIA",
+      },
+      {
+        name: {
+          fname: "JILLIAN",
+          mname: "",
+          lname: "HAN",
+        },
+        address: "TOKYO, JAPAN",
+      },
+      {
+        name: {
+          fname: "NATHANIEL",
+          mname: "E",
+          lname: "UY",
+        },
+        address: "SAPPORO, JAPAN",
+      },
+      {
+        name: {
+          fname: "MARIA LUISA",
+          mname: "R.",
+          lname: "TINAJA",
+        },
+        address: "MADRID, SPAIN",
+      },
+      {
+        name: {
+          fname: " JEA",
+          mname: "P",
+          lname: "ORING",
+        },
+        address: "PARIS, FRANCE",
+      },
+      {
+        name: {
+          fname: " ANNA",
+          mname: "",
+          lname: "ANIB",
+        },
+        address: "BERLIN, GERMANY",
+      },
+      {
+        name: {
+          fname: " GELO",
+          mname: "",
+          lname: "HIBAYA",
+        },
+        address: "ROME, ITALY",
+      },
+      {
+        name: {
+          fname: "MELROSE",
+          mname: "D",
+          lname: "VALIENTE",
+        },
+        address: "BARCELONA, SPAIN",
+      },
+      {
+        name: {
+          fname: " TASH",
+          mname: "",
+          lname: "RAN",
+        },
+        address: "OSLO, NORWAY",
+      },
+      {
+        name: {
+          fname: " JOHNMARK",
+          mname: "Q",
+          lname: "LAGRIMAS",
+        },
+        address: "HELSINKI, FINLAND",
+      },
+      {
+        name: {
+          fname: " JESS",
+          mname: "",
+          lname: "TONG",
+        },
+        address: "STOCKHOLM, SWEDEN",
+      },
+      {
+        name: {
+          fname: " LUI",
+          mname: "R",
+          lname: "LIM",
+        },
+        address: " PERM, RUSSIA",
+      },
+      {
+          name: {
+            fname: "ALBERT",
+            mname: "D",
+            lname: "ANIB",
+          },
+          address: "URUMQI, CHINA",
+        },
+        {
+          name: {
+            fname: "PEARL",
+            mname: "",
+            lname: "TAN",
+          },
+          address: "DUND-US, MONGOLIA",
+        },
+        {
+          name: {
+            fname: "JILLIAN",
+            mname: "",
+            lname: "HAN",
+          },
+          address: "TOKYO, JAPAN",
+        },
+        {
+          name: {
+            fname: "NATHANIEL",
+            mname: "E",
+            lname: "UY",
+          },
+          address: "SAPPORO, JAPAN",
+        },                     
+    
+  ];
+   
